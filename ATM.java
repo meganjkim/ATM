b4e3c938c73a62fc9fc6323e4a5ffd44eed76f0c
meganjkim <megankim1221@gmail.com>
2023-09-07 00:56:14 -0700

    added closeAccount function

---
+++ ATM.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class ATM {
+    private HashMap<String, Double> accounts = new HashMap<String, Double>();
+
+    public ATM() {
+        accounts = new HashMap<String, Double>();
+    }
+
+    public void openAccount(String userId, double amount) throws Exception {
+        if (accounts.containsKey(userId)) {
+            throw new Exception("Error: User already exists.");
+        }
+        accounts.put(userId, amount);
+    }
+
+    public void closeAccount(String userId) {
+        return;
+    }
+
+    public double checkBalance(String userId) {
+        return 1;
+    }
+
+    public double depositMoney(String userId, double amount) {
+        return 1;
+    }
+
+    public double withdrawMoney(String userId, double amount) {
+        return 1;
+    }
+
+    public boolean transferMoney(String fromAccount, String toAccount, double amount) {
+        return false;
+    }
+
+    public void audit() {
+        return;
+    }
+
+}
